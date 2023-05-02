f62d0f7998db4213180409dd302cb30c397146af
arafataft <arafatcd@gmail.com>
2023-05-02 14:08:05 +0600

    navbar add

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import MenuBar from '../pages/Shared/MenuBar/MenuBar';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <MenuBar></MenuBar>
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Shared/MenuBar/MenuBar.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Button, Image, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const MenuBar = () => {
+    return (
+        <div>
+            <Navbar className='container p-2' expand="sm">
+                <Navbar.Brand ><Link to="/" className="mx-3 text-black text-decoration-none">Thai Chef Recipe</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Link to="/" className="mx-3 text-decoration-none">Home</Link>
+                        <Link to='/blog' className="mx-3 text-decoration-none">Blog</Link>
+                    </Nav>
+                    <div><Image src='https://picsum.photos/200' roundedCircle height={25} /></div>
+                    <Button variant="primary">Login</Button>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default MenuBar;
