93ff4e2254407513fe84c9b7402b4829bb498189
arafataft <arafatcd@gmail.com>
2023-05-03 15:47:54 +0600

    add image in errorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import { Link, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
   const { error, status } = useRouteError()
   return (
     <section className='items-center'>
-      <div className='container  items-center px-5 mx-auto my-5'>
+      <div className='container  items-center px-5 mx-auto m-5 '>
+      <div className='d-flex justify-content-center align-items-center'>
+      <Image src="https://img.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_114360-1900.jpg?" alt="" height={400} />
+      </div>
         
         <div className=' text-center'>
           <h2 >
