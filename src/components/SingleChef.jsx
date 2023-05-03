5ceffa40a9408047f92eebfcf8d843c387fbf1b2
arafataft <arafatcd@gmail.com>
2023-05-03 13:57:36 +0600

    single chef details

--- src/components/SingleChef.jsx
+++ src/components/SingleChef.jsx
@@ -1,10 +1,43 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 
-const SingleChef = () => {
+const SingleChef = ({Chef}) => {
+    const {name, image, experience, numOfLike, description, numOfRecipe}=Chef;
+
     return (
-        <div>
-            
+        <div className='container'>
+    <Card className="shadow border p-3">
+      <Row>
+        <Col md={6} className="d-flex justify-content-center align-items-center">
+          <Card.Img variant="top" src={image} style={{ width: '350px', height: '300px' }} />
+        </Col>
+        <Col md={6}>
+          <Card.Body className="d-flex align-items-center">
+            <div>
+              <Card.Title><h2 className='fw-bold'>{name}</h2></Card.Title>
+              <Card.Text>
+              <span className='fw-bold'>Description:</span> {description}
+              </Card.Text>
+              <Card.Text>
+               <span className='fw-bold'> Likes:</span>  {numOfLike}
+                
+              </Card.Text>
+              <Card.Text>
+                <span className='fw-bold'> Recipes:</span>  {numOfRecipe}
+                
+              </Card.Text>
+              <Card.Text>
+                <span className='fw-bold'>Experience:</span>  {experience}
+                
+              </Card.Text>
+            </div>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
+
         </div>
     );
 };
--- src/pages/ChefDetails/ChefDetails.jsx
+++ src/pages/ChefDetails/ChefDetails.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import SingleChef from '../../components/SingleChef';
 
 const ChefDetails = () => {
     const ChefDetailsInfo = useLoaderData();
     let {id} = useParams();
 
-    const chef=ChefDetailsInfo.find(info=>info.id==id);
-    console.log(chef);
-    const {name, image, experience, numOfLike, description, recipes, numOfRecipe}=chef
+    const Chef=ChefDetailsInfo.find(info=>info.id==id);
+
+
+    const { recipes }=Chef;
 
     return (
         <div>
-            
+            <SingleChef Chef={Chef}></SingleChef>
         </div>
     );
 };
