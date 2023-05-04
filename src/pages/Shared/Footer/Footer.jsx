74c93863fadae762fb65b3d2a354b7372cef2993
arafataft <arafatcd@gmail.com>
2023-05-04 12:09:23 +0600

    make footer

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,12 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const Footer = () => {
-    return (
-        <div>
-            <p className='text-center'><small>copyright</small></p>
-        </div>
-    );
-};
+function Footer() {
+  return (
+    <footer className="bg-dark text-light py-5">
+      <Container>
+        <Row>
+          <Col md={4}>
+            <h5>About Chef Hunter</h5>
+            <p>Chef Hunter is a website dedicated to helping foodies discover new restaurants and chefs in their area.</p>
+          </Col>
+          <Col md={4}>
+            <h5>Contact Us</h5>
+            <p>Email: info@chefhunter.com<br />Phone: 555-555-5555</p>
+          </Col>
+          <Col md={4}>
+            <h5>Join Our Newsletter</h5>
+            <p>Sign up to receive updates on the latest restaurant and chef news in your area.</p>
+            <form>
+              <div className="input-group mb-3">
+                <input type="email" className="form-control" placeholder="Enter your email" />
+                <button className="btn btn-light" type="submit">Subscribe</button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col>
+            <p className="text-center">&copy; 2023 Chef Hunter. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
 
 export default Footer;
