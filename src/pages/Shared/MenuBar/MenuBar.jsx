afac0e185345be687d8d474fdd1c099845a63b22
arafataft <arafatcd@gmail.com>
2023-05-04 19:17:11 +0600

    Active Route

--- src/pages/Shared/MenuBar/MenuBar.jsx
+++ src/pages/Shared/MenuBar/MenuBar.jsx
@@ -1,39 +1,64 @@
 /* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
 import { Button, Image, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../../Providers/AuthProvider';
 
 const MenuBar = () => {
+  const { user, logOut } = useContext(AuthContext);
+  const location = useLocation();
 
-    const {user,logOut}=useContext(AuthContext);
+  const handleLogout = () => {
+    logOut()
+      .then()
+      .catch((error) => console.error(error));
+  };
 
-    const handleLogout=()=>{
-        logOut()
-        .then()
-        .catch(error=>console.error(error))
-    }
+  return (
+    <div>
+      <Navbar className='container p-2' expand='sm'>
+        <Navbar.Brand>
+          <Link to='/' className='mx-3 text-black text-decoration-none fw-bolder'>
+            Thai Chef Recipe
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mx-auto'>
+            <Link
+              to='/'
+              className={`mx-3 text-decoration-none ${isActiveLink('/') ? 'fw-bold' : ''}`}>
+              Home
+            </Link>
 
+            <Link
+              to='/blog'
+              className={`mx-3 text-decoration-none ${isActiveLink('/blog') ? 'fw-bold' : ''}`}>
+              Blog
+            </Link>
+          </Nav>
+          {user ? (
+            <div className='d-flex'>
+              <div data-toggle='tooltip' title={user.displayName}>
+                <Image src='https://picsum.photos/200' roundedCircle height={25} />
+              </div>
+              <Button onClick={handleLogout} variant='primary' className='ms-1'>
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <Link to='/login'>
+              <Button variant='primary'>Login</Button>
+            </Link>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 
-    return (
-        <div>
-            <Navbar className='container p-2' expand="sm">
-                <Navbar.Brand ><Link to="/" className="mx-3 text-black text-decoration-none fw-bolder">Thai Chef Recipe</Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mx-auto">
-                        <Link to='/' className="mx-3 text-decoration-none">Home</Link>
-                        <Link to='/blog' className="mx-3 text-decoration-none">Blog</Link>
-                    </Nav>
-                    {user?<div className='d-flex'>
-                        <div  data-toggle='tooltip' title={user.displayName}><Image src='https://picsum.photos/200' roundedCircle height={25} /></div>
-                        <Button onClick={handleLogout} variant="primary" className='ms-1'>Logout</Button>
-                    </div>:
-                    <Link to='/login'><Button variant="primary">Login</Button></Link>}
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    );
+  function isActiveLink(path) {
+    return location.pathname === path;
+  }
 };
 
 export default MenuBar;
