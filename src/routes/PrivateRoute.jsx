7cea43c70bdc55a87bf107fafb0b3d3ae4eb3154
arafataft <arafatcd@gmail.com>
2023-05-04 09:40:03 +0600

    navigate to login previous page

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,14 +2,16 @@
 /* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const {user}=useContext(AuthContext);
+
+    const location=useLocation();
     if(user){
         return children;
     }
-    return <Navigate to='/login'></Navigate>;
+    return <Navigate state={{from:location}} to='/login'></Navigate>;
 };
 
 export default PrivateRoute;
