f3062970d9329cdff427b04da00da1a0e387273d
arafataft <arafatcd@gmail.com>
2023-06-15 17:10:55 +0600

    aos add on home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../../components/Banner';
 import AllChef from '../../components/AllChef/AllChef';
 import Gallery from '../../components/Gallery/Gallery';
 import Catering from '../../components/Catering';
+import Aos from 'aos';
 
 const Home = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1000, // animation duration
+            easing: 'ease-in-out', // animation easing
+            once: true // whether animation should execute only once
+        });
+    }, []);
     return (
         <div>
             <Banner></Banner>
